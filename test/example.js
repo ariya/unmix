a9c9afd21ed661bd71b1674e418d0f0782474011
Rukanda Faridsi <rukanda.faridsi@bukalapak.com>
2018-11-09 13:43:35 +0700

    Update example using localization

--- test/example.js
+++ test/example.js
@@ -1,3 +1,12 @@
 var unmix = require('unmix');
+
 var str = 'Jadi gini guys. Even, kita actually nggak nge-create something gitu lho';
+console.log(unmix.undo(str));
+
+// using locale example
+
+var unmixBekasi = require('unmix/src/locale/unmix.bekasi');
+unmix.localize(unmixBekasi);
+
+var str = 'He eh, aku sih setuju aja';
 console.log(unmix.undo(str));
